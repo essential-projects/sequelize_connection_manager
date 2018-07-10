b375c6d985e820b847569826c5031e4aa948c933
ElRaptorus <christian.werner@5minds.de>
2018-07-10 13:53:12 +0200

    Convert connection factory into single exported function

--- src/binding_factory.ts
+++ src/binding_factory.ts
@@ -7,65 +7,46 @@ const logger: Logger = Logger.createLogger('sequelize_connection_manager:binding
 const connections: {[hash: string]: Sequelize.Sequelize} = {};
 
 /**
-* The factory for creating database connections via Sequelize.
-*
-* @class BindingFactory
-*/
-export class BindingFactory {
+ * Returns a sequelize connection for the current configuration.
+ *
+ * @method getConnection
+ * @param  {String} database The name of the database to connect to
+ * @param  {String} username The username with which to connect to the database
+ * @param  {String} password The password with which to connect to the database
+ * @param  {Object} config   A set of optional configs used for the connection (like dialect, supportBigNumbers, etc)
+ * @return {Object}          The connection for the passed configuration.
+ */
+export function getConnection(database: string, username: string, password: string, config: Sequelize.Options): Promise<Sequelize.Sequelize> {
 
- /**
-  * Checks if an active Connection with the specified config exists.
-  *
-  * @method connectionExistsByHash
-  * @param  {String}  hash The hash-code of the connection to look for.
-  * @return {Boolean}      True, if the connection exists, otherwise false.
-  */
-  public connectionExists(hash: string): boolean {
-   return typeof connections[hash] !== 'undefined';
+  const hash: string = _getHash(database, username, password);
+
+  const connectionExists: boolean = typeof connections[hash] !== 'undefined';
+
+  if (connectionExists) {
+    logger.verbose(`Active postgres connection to '${database}' found.`);
+
+    return Promise.resolve(connections[hash]);
   }
+  const connection: Sequelize.Sequelize = new Sequelize(database, username, password, config);
+  logger.verbose(`postgres connection to database '${database}' established.`);
+  connections[hash] = connection;
 
- /**
-  * Returns a sequelize connection for the current configuration.
-  *
-  * @method getConnection
-  * @param  {String} database The name of the database to connect to
-  * @param  {String} username The username with which to connect to the database
-  * @param  {String} password The password with which to connect to the database
-  * @param  {Object} config   A set of optional configs used for the connection (like dialect, supportBigNumbers, etc)
-  * @return {Object}          The connection for the passed configuration.
-  */
-  public async getConnection(database: string, username: string, password: string, config: Sequelize.Options): Promise<Sequelize.Sequelize> {
+  return Promise.resolve(connection);
+}
 
-      const hash: string = this._getHash(database, username, password);
+/**
+ * Generates a hash from config settings marking a unique connection.
+ *
+ * @method _getHash
+ * @private
+ * @param  {String} database The name of the database to connect to
+ * @param  {String} username The username with which to connect to the database
+ * @param  {String} password The password with which to connect to the database
+ * @return {String} The generated hash.
+ */
+function _getHash(database: string, username: string, password: string): string {
+  const hash: crypto.Hash = crypto.createHash('sha256');
+  const properties: string = `${database}${username}${password}`;
 
-      const connectionExists: boolean = this.connectionExists(hash);
-
-      if (connectionExists) {
-        logger.verbose(`Active postgres connection to '${database}' found.`);
-
-        return Promise.resolve(connections[hash]);
-      }
-      const connection: Sequelize.Sequelize = new Sequelize(database, username, password, config);
-      logger.verbose(`postgres connection to database '${database}' established.`);
-      connections[hash] = connection;
-
-      return Promise.resolve(connection);
-  }
-
- /**
-  * Generates a hash from config settings marking a unique connection.
-  *
-  * @method _getHash
-  * @private
-  * @param  {String} database The name of the database to connect to
-  * @param  {String} username The username with which to connect to the database
-  * @param  {String} password The password with which to connect to the database
-  * @return {String} The generated hash.
-  */
-  private _getHash(database: string, username: string, password: string): string {
-    const hash: crypto.Hash = crypto.createHash('sha256');
-    const properties: Array<string> = [database, username, password];
-
-    return hash.update(properties.join('')).digest('hex');
-  }
+  return hash.update(properties).digest('hex');
 }
